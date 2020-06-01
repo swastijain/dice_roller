f59ee838686946b637c85c499d7a5193551ce35b
Vaibhav <vaibhavjain30699@gmail.com>
2020-06-01 14:35:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/vaibhav/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.vaibhav.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage : ImageView
+    private lateinit var numberDisplay : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
+        numberDisplay = findViewById(R.id.number_display)
+
+        val rollButton : Button = findViewById(R.id.roll_button);
+        rollButton.setOnClickListener {rollDice()}
+    }
+
+    private fun rollDice(){
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        setNumber(number = randomInt)
+    }
+
+    private fun setNumber(number : Int){
+        numberDisplay.text = "Number is: $number"
+    }
+
+}
