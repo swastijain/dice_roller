7561864fd7bf84f46e28ddc02cfa30ca3fc7b424
Vaibhav <vaibhavjain30699@gmail.com>
2020-10-08 18:01:47 +0530

    Removed the one dice number

--- app/src/main/java/com/vaibhav/myapplication/MainActivity.kt
+++ app/src/main/java/com/vaibhav/myapplication/MainActivity.kt
@@ -27,13 +27,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice(){
-        val randomInt = (1..6).random()
+        val randomInt = (1..5).random()
         val drawableResource = when (randomInt){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
         diceImage.setImageResource(drawableResource)
