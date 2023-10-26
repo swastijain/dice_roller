793b9737515ce9ec152ea96be364627818bda96a
Vaibhav Jain <vaibhavjain30699@gmail.com>
2023-10-26 23:41:30 +0530

    [Fix] Number 5 showing image for number 4

--- app/src/main/java/com/vaibhav/myapplication/MainActivity.kt
+++ app/src/main/java/com/vaibhav/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
         diceImage.setImageResource(drawableResource)
